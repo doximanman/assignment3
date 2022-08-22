ad82d0c02f967004cad13c278fdda64ad84997f0
doximanman <kalo39570@gmail.com>
2022-08-22 20:10:57 +0300

    Fixed a bug

--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -70,8 +70,8 @@ void TCPServer::handleClient() {
         // classifies the data using euclidean distance.
         map<string,vector<Point>> data = CSVManagement::getClassifiedData(fileHandler::getLines(_dataPath));
         vector<Point> unclassified=CSVManagement::getUnclassifiedData(unclassifiedPoints);
-        //EuclideanDistance euclideanDistance{};
-        KNearestNeighbors knn(data,K,(EuclideanDistance()));
+        EuclideanDistance euclideanDistance{};
+        KNearestNeighbors knn(data,K,euclideanDistance);
         vector<string> classifiedData = knn.classifyData(unclassified);
         //combines the classified data into one string (with ' ' (space) as separation between classifications).
         for (int i = 0; i < classifiedData.size(); i++) {
