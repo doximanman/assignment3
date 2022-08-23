0c20cb05e4326e4a638ce856725f4a652c20d664
doximanman <kalo39570@gmail.com>
2022-08-23 18:17:40 +0300

    Even More Commands!

---
+++ src/Commands/DownloadResults.hpp
@@ -0,0 +1,15 @@
+#ifndef SIMPLE_EXAMPLE_DOWNLOADRESULTS_HPP
+#define SIMPLE_EXAMPLE_DOWNLOADRESULTS_HPP
+
+#include "Classifier.hpp"
+
+class DownloadResults : Command {
+private:
+    Classifier _cl;
+public:
+    DownloadResults(DefaultIO& dio,Classifier cl);
+    void execute() override;
+};
+
+
+#endif //SIMPLE_EXAMPLE_DOWNLOADRESULTS_HPP
