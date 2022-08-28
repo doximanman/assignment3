48a6cb6eb65b6153263f6b3b08defd7294494463
EinatNoyman <einat2004@gmail.com>
2022-08-29 01:38:52 +0300

    Start ConfusionMatrix.cpp

--- src/Commands/ConfusionMatrix.cpp
+++ src/Commands/ConfusionMatrix.cpp
@@ -2,24 +2,39 @@
 
 #include <utility>
 
-ConfusionMatrix::ConfusionMatrix(DefaultIO &dio, Classifier& cl) : Command(dio),_cl(cl) {
-    _description="display algorithm confusion matrix";
+ConfusionMatrix::ConfusionMatrix(DefaultIO &dio, Classifier &cl) : Command(dio), _cl(cl) {
+    _description = "display algorithm confusion matrix";
 }
 
 void ConfusionMatrix::clear() {
     _cl.clear();
-    _knownClassifications.clear();
+    _realClassifications.clear();
 }
 
-void ConfusionMatrix::addPoints(const std::vector<Geometry::Point>& points) {
+void ConfusionMatrix::addPoints(const std::vector<Geometry::Point> &points) {
     _cl.addPoints(points);
 }
 
-void ConfusionMatrix::addClassifications(const std::vector<std::string>& classifications) {
-    for(const std::string& classification : classifications){
-        _knownClassifications.push_back(classification);
+void ConfusionMatrix::setN() {
+    std::vector<std::string> typesNames;
+    for(std::string &classification: _realClassifications) {
     }
 }
+
+void ConfusionMatrix::addClassifications(const std::vector<std::string> &classifications) {
+    for (const std::string &classification: classifications) {
+        _realClassifications.push_back(classification);
+    }
+}
+
+void ConfusionMatrix::calculateClassifications() {
+
+}
+
+void ConfusionMatrix::printMatrix() {
+
+}
+
 void ConfusionMatrix::execute() {
     // implementation needed.
     // code should fail if points haven't been classified yet (using the '3' command).
--- src/Commands/ConfusionMatrix.hpp
+++ src/Commands/ConfusionMatrix.hpp
@@ -3,15 +3,31 @@
 
 #include "Classifier.hpp"
 
-class ConfusionMatrix : public Command{
+class ConfusionMatrix : public Command {
 private:
-    Classifier& _cl;
-    std::vector<std::string> _knownClassifications;
+    Classifier &_cl;
+    std::vector<std::string> _realClassifications;
+    std::vector<std::string> _calculatedClassifications;
+    int _N;
+    int **_matrix;
+
+    int successRates(std::string real, std::string classified);
+
+    void calculateClassifications();
+
+    void printMatrix();
+
+    void setN();
+
 public:
-    ConfusionMatrix(DefaultIO& dio,Classifier& cl);
+    ConfusionMatrix(DefaultIO &dio, Classifier &cl);
+
     void clear();
-    void addPoints(const std::vector<Geometry::Point>& points);
-    void addClassifications(const std::vector<std::string>& classifications);
+
+    void addPoints(const std::vector<Geometry::Point> &points);
+
+    void addClassifications(const std::vector<std::string> &classifications);
+
     void execute() override;
 };
 
